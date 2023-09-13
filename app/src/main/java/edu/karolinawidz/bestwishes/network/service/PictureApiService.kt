9a3bfe76f3d44089f65db4a445591f1621bc3ba2
Karolina Widz <karolinawidz97@gmail.com>
2023-09-13 20:55:41 +0200

    Remove unnecessary code

--- app/src/main/java/edu/karolinawidz/bestwishes/network/service/PictureApiService.kt
+++ app/src/main/java/edu/karolinawidz/bestwishes/network/service/PictureApiService.kt
@@ -1,25 +1,12 @@
 package edu.karolinawidz.bestwishes.network.service
 
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import edu.karolinawidz.bestwishes.BuildConfig
 import edu.karolinawidz.bestwishes.network.model.Data
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Query
 
 private const val KEY = BuildConfig.PEXELS_KEY
-private const val BASE_URL = "https://api.pexels.com/v1/"
-private val moshi = Moshi.Builder()
-    .add(KotlinJsonAdapterFactory())
-    .build()
-
-private val retrofit = Retrofit.Builder()
-    .addConverterFactory(MoshiConverterFactory.create(moshi))
-    .baseUrl(BASE_URL)
-    .build()
 
 fun interface PictureApiService {
     @Headers("Authorization: $KEY")
@@ -27,8 +14,3 @@ fun interface PictureApiService {
     suspend fun getPictures(@Query("page") page: Int, @Query("query") category: String): Data
 }
 
-object PictureApi {
-    val retrofitService: PictureApiService by lazy {
-        retrofit.create(PictureApiService::class.java)
-    }
-}
